8f484769ffc097bda4bd654e383de23a01de98ca
cadriansalazarg <cadriansalazarg@gmail.com>
2020-04-14 21:28:42 -0600

    Update prll_bs_gnrtr_n_rbtr_wrap_SV.sv

--- Uso_Bus_Paralelo_Simple/vivado/src_Verilog/prll_bs_gnrtr_n_rbtr_wrap_SV.sv
+++ Uso_Bus_Paralelo_Simple/vivado/src_Verilog/prll_bs_gnrtr_n_rbtr_wrap_SV.sv
@@ -25,16 +25,16 @@
 module prll_bs_gnrtr_n_rbtr_wrap_SV #(parameter buses = 1,parameter bits = 32,parameter drvrs = 2, parameter broadcast = {8{1'b1}}) (    
     input clk,
     input reset,
-    input  pndng_0_0,
-    input  pndng_0_1,
-    output push_0_0,
-    output push_0_1,
-    output pop_0_0,
-    output pop_0_1,
-    input  [bits-1:0] D_pop_0_0,
-    input  [bits-1:0] D_pop_0_1,
-    output [bits-1:0] D_push_0_0,
-    output [bits-1:0] D_push_0_1
+    input  pndng_drvr_0_bus_0,
+    input  pndng_drvr_1_bus_0,
+    output push_drvr_0_bus_0,
+    output push_drvr_1_bus_0,
+    output pop_drvr_0_bus_0,
+    output pop_drvr_1_bus_0,
+    input  [bits-1:0] D_pop_drvr_0_bus_0,
+    input  [bits-1:0] D_pop_drvr_1_bus_0,
+    output [bits-1:0] D_push_drvr_0_bus_0,
+    output [bits-1:0] D_push_drvr_1_bus_0
  );
     
     
@@ -44,20 +44,20 @@ module prll_bs_gnrtr_n_rbtr_wrap_SV #(parameter buses = 1,parameter bits = 32,pa
     wire [bits-1:0] D_pop [buses-1:0][drvrs-1:0];
     wire [bits-1:0] D_push [buses-1:0][drvrs-1:0];
     
-    assign pndng[0][0] = pndng_0_0;
-    assign pndng[0][1] = pndng_0_1;
+    assign pndng[0][0] = pndng_drvr_0_bus_0;
+    assign pndng[0][1] = pndng_drvr_1_bus_0;
     
-    assign push_0_0 = push[0][0];
-    assign push_0_1 = push[0][1];
+    assign push_drvr_0_bus_0 = push[0][0];
+    assign push_drvr_1_bus_0 = push[0][1];
     
-    assign pop_0_0 = pop[0][0];
-    assign pop_0_1 = pop[0][1];
+    assign pop_drvr_0_bus_0 = pop[0][0];
+    assign pop_drvr_1_bus_0 = pop[0][1];
     
-    assign D_pop[0][0] = D_pop_0_0;
-    assign D_pop[0][1] = D_pop_0_1;
+    assign D_pop[0][0] = D_pop_drvr_0_bus_0;
+    assign D_pop[0][1] = D_pop_drvr_1_bus_0;
     
-    assign D_push_0_0 = D_push[0][0];
-    assign D_push_0_1 = D_push[0][1];
+    assign D_push_drvr_0_bus_0 = D_push[0][0];
+    assign D_push_drvr_1_bus_0 = D_push[0][1];
     
     prll_bs_gnrtr_n_rbtr #(.buses(buses),.bits(bits),.drvrs(drvrs),.broadcast(broadcast)) bus_interfase (
     .clk(clk),
