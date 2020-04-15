07235869fb9f68e551719c7a14c4131ba366ba5b
cadriansalazarg <cadriansalazarg@gmail.com>
2020-04-14 22:08:44 -0600

    Update customized_IP.cpp

--- Uso_Bus_Paralelo_Simple/hls/customized_IP.cpp
+++ Uso_Bus_Paralelo_Simple/hls/customized_IP.cpp
@@ -9,17 +9,17 @@ void customized_IP_block( hls::stream<data_t>& out_fifo_drvr_0,
                      data_t input_port_axi_lite_drvr_0[SIZE],
                      data_t input_port_axi_lite_drvr_1[SIZE]){				
     
-    Loop1: for (int i=0;i<SIZE;i++) { // Se envían los datos hacia el fifo
+    Loop1: for (int i=0;i<SIZE;i++) { 
 		out_fifo_drvr_0.write(input_port_axi_lite_drvr_0[i]); 
 		out_fifo_drvr_1.write(input_port_axi_lite_drvr_1[i]);
     } 
     
-    Loop2: for (int i=0;i<SIZE;i++) { // Se envían los datos hacia el fifo
+    Loop2: for (int i=0;i<SIZE;i++) { 
 		while(in_fifo_drvr_1.empty());
 		output_port_axi_lite_drvr_1[i] = in_fifo_drvr_1.read();
     } 
     
-    Loop3: for (int i=0;i<SIZE;i++) { // Se envían los datos hacia el fifo
+    Loop3: for (int i=0;i<SIZE;i++) { 
 		while(in_fifo_drvr_0.empty());
 		output_port_axi_lite_drvr_0[i] = in_fifo_drvr_0.read();
     } 
