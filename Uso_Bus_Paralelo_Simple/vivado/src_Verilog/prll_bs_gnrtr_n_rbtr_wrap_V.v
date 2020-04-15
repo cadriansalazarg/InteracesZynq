64654bc9add0294f64e84847c5223f3c2d753f56
cadriansalazarg <cadriansalazarg@gmail.com>
2020-04-14 21:27:54 -0600

    Update prll_bs_gnrtr_n_rbtr_wrap_V.v

--- Uso_Bus_Paralelo_Simple/vivado/src_Verilog/prll_bs_gnrtr_n_rbtr_wrap_V.v
+++ Uso_Bus_Paralelo_Simple/vivado/src_Verilog/prll_bs_gnrtr_n_rbtr_wrap_V.v
@@ -23,31 +23,31 @@
 module prll_bs_gnrtr_n_rbtr_wrap_V #(parameter buses = 1,parameter bits = 32,parameter drvrs = 2, parameter broadcast = {8{1'b1}}) (
     input clk,
     input reset,
-    input  pndng_0_0,
-    input  pndng_0_1,
-    output push_0_0,
-    output push_0_1,
-    output pop_0_0,
-    output pop_0_1,
-    input  [bits-1:0] D_pop_0_0,
-    input  [bits-1:0] D_pop_0_1,
-    output [bits-1:0] D_push_0_0,
-    output [bits-1:0] D_push_0_1
+    input  pndng_drvr_0_bus_0,
+    input  pndng_drvr_1_bus_0,
+    output push_drvr_0_bus_0,
+    output push_drvr_1_bus_0,
+    output pop_drvr_0_bus_0,
+    output pop_drvr_1_bus_0,
+    input  [bits-1:0] D_pop_drvr_0_bus_0,
+    input  [bits-1:0] D_pop_drvr_1_bus_0,
+    output [bits-1:0] D_push_drvr_0_bus_0,
+    output [bits-1:0] D_push_drvr_1_bus_0
  );
     
     prll_bs_gnrtr_n_rbtr_wrap_SV #(.buses(buses),.bits(bits),.drvrs(drvrs),.broadcast(broadcast)) bus_interfase (
     .clk(clk),
     .reset(reset),
-    .pndng_0_0(pndng_0_0),
-    .pndng_0_1(pndng_0_1),
-    .push_0_0(push_0_0),
-    .push_0_1(push_0_1),
-    .pop_0_0(pop_0_0),
-    .pop_0_1(pop_0_1),
-    .D_pop_0_0(D_pop_0_0),
-    .D_pop_0_1(D_pop_0_1),
-    .D_push_0_0(D_push_0_0),
-    .D_push_0_1(D_push_0_1)
+    .pndng_drvr_0_bus_0(pndng_drvr_0_bus_0),
+    .pndng_drvr_1_bus_0(pndng_drvr_1_bus_0),
+    .push_drvr_0_bus_0(push_drvr_0_bus_0),
+    .push_drvr_1_bus_0(push_drvr_1_bus_0),
+    .pop_drvr_0_bus_0(pop_drvr_0_bus_0),
+    .pop_drvr_1_bus_0(pop_drvr_1_bus_0),
+    .D_pop_drvr_0_bus_0(D_pop_drvr_0_bus_0),
+    .D_pop_drvr_1_bus_0(D_pop_drvr_1_bus_0),
+    .D_push_drvr_0_bus_0(D_push_drvr_0_bus_0),
+    .D_push_drvr_1_bus_0(D_push_drvr_1_bus_0)
     );
     
 endmodule
