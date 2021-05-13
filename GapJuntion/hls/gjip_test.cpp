b7b664dafc9ed9e1b1d8eda39bccd87e46d88e7b
manchii <kaleb.23415@gmail.com>
2021-05-12 20:47:08 -0600

    hls gjip with ZynqInterfaces

---
+++ GapJuntion/hls/gjip_test.cpp
@@ -0,0 +1,91 @@
+#include "gjip.hpp"
+#include <iostream>
+
+const int size_cell_population = 32;
+
+void setConfigSim(Config &config, int population){
+	config.population = population;
+	config.start_limit = 0;
+	config.end_limit = population;
+	config.fpga_id = 1;
+	config.uid = 1;
+	config.bus_id = 1;
+}
+
+
+void fillVdend(AXIS_interface_in &input,int population){
+	for(int i=0; i<population; i+=2){
+		packaging_data p;
+		float value = -50.0f;
+		p.MESSAGE[0]= *reinterpret_cast<int *>(&value);
+		p.MESSAGE[1]= *reinterpret_cast<int *>(&value);
+		p.MESSAGE[2]= *reinterpret_cast<int *>(&value);
+		p.MESSAGE[3]= *reinterpret_cast<int *>(&value);
+		p.MESSAGE[4]= *reinterpret_cast<int *>(&value);
+		p.MESSAGE[5]= *reinterpret_cast<int *>(&value);
+		p.BS_ID = 1;
+		p.FPGA_ID = 1;
+		p.RX_UID = 1;
+		p.TX_UID = 2;
+		p.PCKG_ID = i;
+		p.VALID_PACKET_BYTES = VALID_PACKET_BYTES;
+		input.write(p);
+	}
+}
+
+//template<typename T, typename S>
+//void SimulateSW(const T &C,const T &V, S &output_Software) {
+//        const float hundred = -1.0f / 100.0f;
+//        const auto size = V.size();
+//        auto k = 0;
+//        for (int i = 0; i < size; i++) {
+//                auto f_acc = 0.0f;
+//                auto v_acc = 0.0f;
+//                for (int j = 0; j < size; j++) {
+//                        auto v = V.data()[i] - V.data()[j];
+//                        auto f = v * expf(v * v * hundred);
+//                        f_acc += f * C.data()[k];
+//                        v_acc += v * C.data()[k];
+//                        k++;
+//                }
+//                auto I_c = (0.8f * f_acc + 0.2f * v_acc);
+//                output_Software.write(I_c);
+//        }
+//}
+
+void readIcurr(AXIS_interface_out &output, int population){
+	int count=0;
+	int id = 0;
+	while(!output.empty()){
+		auto p = output.read();
+		auto &message = p.MESSAGE;
+		float *data = reinterpret_cast<int*>&message;
+		std::cout<<
+				"Out: "<<data[i]<<"\n"<<
+				"Out: "<<data[i+1]<<"\n"<<
+				"Out: "<<data[i+2]<<"\n"<<
+				"Out: "<<data[i+3]<<"\n"<<
+				"Out: "<<data[i+4]<<"\n"<<
+				"Out: "<<data[i+5]<<"\n"<<
+		count += 6;
+	}
+	std::cout<< "received data: "<< count << "\n";
+}
+
+int main(int argc, char** argv){
+
+	std::cout<< "Simulation initialization\n";
+
+	static AXIS_interface_in input;
+	static AXIS_interface_out output;
+	static Config config;
+
+	setConfigSim(config,size_cell_population);
+	fillVdend(input, size_cell_population);
+
+	GapJunctionIP(input, output, config);
+
+	readIcurr(output, size_cell_population);
+
+	return 0;
+}
