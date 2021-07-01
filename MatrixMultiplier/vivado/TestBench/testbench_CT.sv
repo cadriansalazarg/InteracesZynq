d6462bd4ceb709f158111b098c6f23f1ab0b6e74
cadriansalazarg <cadriansalazarg@gmail.com>
2021-07-01 03:56:57 -0600

    Se agrega el testbench del centralizador

--- MatrixMultiplier/vivado/TestBench/testbench_CT.sv
+++ MatrixMultiplier/vivado/TestBench/testbench_CT.sv
@@ -1,76 +1,109 @@
 `timescale 1ns / 1ps
 
-module testbench;
-
-    wire Error;
-    wire channel_up;
-    wire clk_200MHz;
-    wire [255:0]dout;
-    wire full;
-    wire gt_refclk;
-    wire init_clk;
-    wire not_empty; 
-    wire [0:0]txn;
-    wire [0:0]txp;
-    wire user_clk;
+module testbench_CT;
+ 
+    wire channel_up_M0;
+    wire channel_up_M1;
+    wire channel_up_M2;
+    wire channel_up_M3;
     
-    wire [0:0]rxn;
-    wire [0:0]rxp;
+    wire channel_up_S1;
+    wire channel_up_S2;
+    wire channel_up_S3;
     
-    reg wr_en;
     reg clk_200MHz_n;
     reg clk_200MHz_p;
-    reg [255:0]din;
-    reg rd_en;
+    
     reg peripheral_reset;
+     
+    wire rxn_0;
+    wire rxn_1;
+    wire rxn_2;
+    wire rxn_3;
     
-    int Q_counter = 0;
-    const bit [7:0] BS_ID_XMULT_0 = 8'h00;
-    const bit [7:0] BS_ID_XMULT_1 = 8'h01;
-    const bit [7:0] BS_ID_AUROR_0 = 8'h03;
-    const bit [7:0] BS_ID_BROAD_0 = 8'hFF;
+    wire rxp_0;
+    wire rxp_1;
+    wire rxp_2;
+    wire rxp_3;
     
-    bit [15:0] Q_packet_id = 0;
-    bit [7:0] FPGA_ID;
-    bit [15:0] PCKG_ID;
-    bit [7:0] TX_UID;
-    bit [7:0] RX_UID;
-    bit [15:0] VALID_PACKET_BYTES;
+    wire txn_0;
+    wire txn_1;
+    wire txn_2;
+    wire txn_3;
+    
+    wire txp_0;
+    wire txp_1;
+    wire txp_2;
+    wire txp_3;
+   
 
 
-    design_1_wrapper uut (
-    .Error(Error),
-    .channel_up(channel_up),
-    .clk_200MHz(clk_200MHz),
+    Master_FPGA1_wrapper Master (
+    .channel_up_0(channel_up_M0),
+    .channel_up_1(channel_up_M1),
+    .channel_up_2(channel_up_M2),
+    .channel_up_3(channel_up_M3),
     .clk_200MHz_n(clk_200MHz_n),
     .clk_200MHz_p(clk_200MHz_p),
-    .din(din),
-    .dout(dout),
-    .full(full),
-    .gt_refclk(gt_refclk),
-    .init_clk(init_clk),
-    .not_empty(not_empty),
     .peripheral_reset(peripheral_reset),
-    .rd_en(rd_en),
-    .rxn(rxn),
-    .rxp(rxp),
-    .txn(txn),
-    .txp(txp),
-    .user_clk(user_clk),
-    .wr_en(wr_en));
+    .rxn_0(rxn_0),
+    .rxn_1(rxn_1),
+    .rxn_2(rxn_2),
+    .rxn_3(rxn_3),
+    .rxp_0(rxp_0),
+    .rxp_1(rxp_1),
+    .rxp_2(rxp_2),
+    .rxp_3(rxp_3),
+    .txn_0(txn_0),
+    .txn_1(txn_1),
+    .txn_2(txn_2),
+    .txn_3(txn_3),
+    .txp_0(txp_0),
+    .txp_1(txp_1),
+    .txp_2(txp_2),
+    .txp_3(txp_3)
+    );
     
-    assign rxn = txn;
-    assign rxp = txp;
+    Slave_FPGA2_wrapper Slave_FPGA1(
+    .channel_up_0(channel_up_S1),
+    .clk_200MHz_n(clk_200MHz_n),
+    .clk_200MHz_p(clk_200MHz_p),
+    .peripheral_reset(peripheral_reset),
+    .rxn_0(txn_0),
+    .rxp_0(txp_0),
+    .txn_0(rxn_0),
+    .txp_0(rxp_0)
+    );
+    
+    Slave_FPGA2_wrapper Slave_FPGA2(
+    .channel_up_0(channel_up_S2),
+    .clk_200MHz_n(clk_200MHz_n),
+    .clk_200MHz_p(clk_200MHz_p),
+    .peripheral_reset(peripheral_reset),
+    .rxn_0(txn_1),
+    .rxp_0(txp_1),
+    .txn_0(rxn_1),
+    .txp_0(rxp_1)
+    );
+    
+    Slave_FPGA2_wrapper Slave_FPGA3(
+    .channel_up_0(channel_up_S3),
+    .clk_200MHz_n(clk_200MHz_n),
+    .clk_200MHz_p(clk_200MHz_p),
+    .peripheral_reset(peripheral_reset),
+    .rxn_0(txn_2),
+    .rxp_0(txp_2),
+    .txn_0(rxn_2),
+    .txp_0(rxp_2)
+    );
+    
+    assign rxn_3 = txn_3;
+    assign rxp_3 = txp_3;
     
     initial begin
-        wr_en = 0;
         clk_200MHz_n = 0;
         clk_200MHz_p = 1;
-        din = 256'd0;
-        rd_en = 0;
-        peripheral_reset = 1;
-        Q_packet_id = 16'd0;
-        
+        peripheral_reset = 1;        
         #16 peripheral_reset = 0;
         
     end
@@ -78,80 +111,4 @@ module testbench;
     always #2.5 clk_200MHz_n = ~clk_200MHz_n;
     always #2.5 clk_200MHz_p = ~clk_200MHz_p;
     
-    
-    
-    always @(posedge clk_200MHz) begin
-        if ((channel_up == 1'b1) && (full == 1'b0)) begin
-            if(Q_counter < 8'd40) begin
-                din = 256'd0;
-                wr_en = 0;
-                Q_counter = Q_counter + 1'b1;
-            end else if (Q_counter < 8'd76) begin // 40 + 36
-                FPGA_ID = 8'h00;
-                PCKG_ID = Q_packet_id;
-                TX_UID = 8'h02;
-                RX_UID = 8'h00;
-                VALID_PACKET_BYTES = 16'd24;
-                din = PacketGenerator(BS_ID_XMULT_0, FPGA_ID, PCKG_ID, TX_UID, RX_UID, VALID_PACKET_BYTES); 
-                wr_en = 1;
-                Q_packet_id = Q_packet_id + 1'b1;
-                Q_counter = Q_counter + 1'b1;
-            end else if (Q_counter < 8'd112) begin // 76 + 36
-                FPGA_ID = 8'h00;
-                PCKG_ID = Q_packet_id;
-                TX_UID = 8'h02;
-                RX_UID = 8'h01;
-                VALID_PACKET_BYTES = 16'd24;
-                din = PacketGenerator(BS_ID_XMULT_1, FPGA_ID, PCKG_ID, TX_UID, RX_UID, VALID_PACKET_BYTES); 
-                wr_en = 1;
-                Q_packet_id = Q_packet_id + 1'b1;
-                Q_counter = Q_counter + 1'b1;
-            end else begin
-                din = 256'd0;
-                wr_en = 0;
-            end
-        end
-    end
-    
-    always @(negedge clk_200MHz) begin
-        if (not_empty) begin
-            rd_en = 1'b1;
-            Read_FIFO_monitor();
-        end else begin
-            rd_en = 1'b0;
-        end    
-    end
-    
-    function bit [255:0] PacketGenerator;
-        input bit [7:0] BS_ID;
-        input bit [7:0] FPGA_ID;
-        input bit [15:0] PCKG_ID;
-        input bit [7:0] TX_UID;
-        input bit [7:0] RX_UID;
-        input bit [15:0] VALID_PACKET_BYTES;
-        
-        if (PCKG_ID == 0) 
-            return {BS_ID, FPGA_ID, PCKG_ID, TX_UID, RX_UID, VALID_PACKET_BYTES, 32'd1, 32'd1, 32'd1, 32'd1, 32'd1, 32'd1};
-        else if (PCKG_ID == 1)   
-            return {BS_ID, FPGA_ID, PCKG_ID, TX_UID, RX_UID, VALID_PACKET_BYTES, 32'd1, 32'd1, 32'd1, 32'd1, 32'd1, 32'd1};
-        else
-            return {BS_ID, FPGA_ID, PCKG_ID, TX_UID, RX_UID, VALID_PACKET_BYTES, 32'd1, 32'd1, 32'd1, 32'd1, 32'd1, 32'd1};
-    endfunction
-    
-    
-    task Read_FIFO_monitor();
-        $display("*****************************************************************************");
-        $display("En el tiempo %t us, se leyó un dato de la FIFO.", $time);
-        $display("El encabezado del paquete es");
-        $display("BS_ID: 0x%0h. FPGA_ID: 0x%0h. PCKG_ID: %d. TX_UID: 0x%0h. RX_UID: 0x%0h. VALID_PACKET_BYTES: 0x%0h.", dout[255:248], dout[247:240], dout[239:224], dout[223:216], dout[215:208], dout[207:192]);
-        $display("El mensaje 0 del paquete es: %d.", dout[191:160]);
-        $display("El mensaje 1 del paquete es: %d.", dout[159:128]);
-        $display("El mensaje 2 del paquete es: %d.", dout[127:96]);
-        $display("El mensaje 3 del paquete es: %d.", dout[95:64]);
-        $display("El mensaje 4 del paquete es: %d.", dout[63:32]);
-        $display("El mensaje 5 del paquete es: %d.", dout[31:0]);
-    endtask
-    
-
-
 endmodule
